5f376767fe56d40e0eb9c77f7ba07be9def00213
dzsears <dsears1@my.athens.edu>
2017-04-01 23:40:41 -0500

    Pushing all files needed to build BioRubebot for team. Fix for cleaning of repository

---
+++ Assets/Scripts/ReloadScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour
+{
+
+    // Use this for restart models
+    public void restartbutton()
+    {
+        SceneManager.LoadScene("Level2");
+        T_RegCmdCtrl.gameWon = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Spring 2017
+Project Repository for the Spring 2017 Team
+This Repository is the working game to be used for uploading onto iPads for Dr. Cline. 
+The Project is currently compiled under Unity Version 5.5.0f3.
